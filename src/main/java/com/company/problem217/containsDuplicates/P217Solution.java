25d2e68dc2a0a52cc5b93633718d19740254f2f0
pushkarchauhan91 <pushkarchauhan91@pushkarchauhan91s-MacBook-Air.local>
2024-06-04 10:01:37 +0530

    Solution : contains duplicate

--- src/main/java/com/company/problem217/containsDuplicates/P217Solution.java
+++ src/main/java/com/company/problem217/containsDuplicates/P217Solution.java
@@ -1,4 +1,4 @@
-package com.company.problem217;
+package com.company.problem217.containsDuplicates;
 
 import java.util.*;
 
@@ -13,6 +13,7 @@ public class P217Solution {
         System.out.println(solution.containsDuplicateUsingSortAndTwoPointer(nums));
     }
 
+    // O(n)
     public boolean containsDuplicateUsingHashMap(int[] nums) {
         Map<Integer, Integer> map = new LinkedHashMap<>();
         for (int i = 0; i < nums.length; i++) {
@@ -24,6 +25,7 @@ public class P217Solution {
         return false;
     }
 
+    // O(n)
     public boolean containsDuplicateUsingHashSet(int[] nums) {
         Set<Integer> set = new LinkedHashSet<>();
         for (int num : nums) {
@@ -35,16 +37,18 @@ public class P217Solution {
         return false;
     }
 
+    // O(nlog(n))
     public boolean containsDuplicateUsingSortAndCompare(int[] nums) {
         Arrays.sort(nums);
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == nums[i + 1]) {
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i - 1] == nums[i]) {
                 return true;
             }
         }
         return false;
     }
 
+    // O(nlog(n))
     public boolean containsDuplicateUsingSortAndTwoPointer(int[] nums) {
         Arrays.sort(nums);
         int left = 0;
--- src/test/java/com/company/problem217/containsDuplicates/P217SolutionTest.java
+++ src/test/java/com/company/problem217/containsDuplicates/P217SolutionTest.java
@@ -1,4 +1,22 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.company.problem217.containsDuplicates;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 class P217SolutionTest {
-  
+
+    @Test
+    void containsDuplicate() {
+        P217Solution solution = new P217Solution();
+        Assertions.assertTrue(solution.containsDuplicateUsingHashMap(new int[]{1, 2, 3, 4, 1}));
+        Assertions.assertTrue(solution.containsDuplicateUsingHashSet(new int[]{1, 2, 3, 4, 1}));
+        Assertions.assertTrue(solution.containsDuplicateUsingSortAndCompare(new int[]{1, 2, 3, 4, 1}));
+        Assertions.assertTrue(solution.containsDuplicateUsingSortAndTwoPointer(new int[]{1, 2, 3, 4, 1}));
+
+        Assertions.assertFalse(solution.containsDuplicateUsingHashMap(new int[]{1, 2, 3, 4}));
+        Assertions.assertFalse(solution.containsDuplicateUsingHashSet(new int[]{1, 2, 3, 4}));
+        Assertions.assertFalse(solution.containsDuplicateUsingSortAndCompare(new int[]{1, 2, 3, 4}));
+        Assertions.assertFalse(solution.containsDuplicateUsingSortAndTwoPointer(new int[]{1, 2, 3, 4}));
+    }
+
 }
